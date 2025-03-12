c0cafa4207cbafc8c865513b99d7dfc179b59d85
pauloromano3738 <pauloromano3738@gmail.com>
2025-03-12 18:42:28 -0300

    adicionando projeto spring boot

---
+++ src/main/java/com/mycarscan/my_car_scan/MyCarScanApplication.java
@@ -0,0 +1,13 @@
+package com.mycarscan.my_car_scan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCarScanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCarScanApplication.class, args);
+	}
+
+}
